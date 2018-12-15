7162233e18f479229443e2df8cdce0b3cccff71a
Logan McAnsh <logan@mcan.sh>
2018-12-14 20:04:39 -0500

    use proper query param on now
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
-const withTypescript = require('@zeit/next-typescript');
+const { PHASE_PRODUCTION_SERVER } =
+  process.env.NODE_ENV === 'development'
+    ? {}
+    : !process.env.NOW_REGION // ℹ️ Must be `NOW_REGION`, not `NOW` (my bad)
+      ? require('next/constants')
+      : require('next-server/constants');
 
-module.exports = withTypescript();
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_PRODUCTION_SERVER) {
+    // Config used to run in production.
+    return {};
+  }
+
+  const withTypescript = require('@zeit/next-typescript');
+
+  return withTypescript()
+};
