adde67d5a7a862f2f55a64f2639e580c776d460b
Logan McAnsh <logan@mcan.sh>
2018-12-27 15:37:10 -0500

    eslint
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,30 @@
 module.exports = {
-  parser: "eslint-plugin-typescript/parser",
-  plugins: ["typescript"],
+  parser: 'pluggable-babel-eslint',
+  env: {
+    es6: true,
+  },
+  plugins: ['jsx-a11y', 'react', 'prettier', 'react-hooks'],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    plugins: ['typescript'],
+  },
   rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react/jsx-filename-extension': [
+      'warn',
+      { extensions: ['.js', '.jsx', '.tsx'] },
+    ],
+    'react/jsx-closing-tag-location': 'off',
+    'react/jsx-curly-brace-presence': 'off',
+    'no-param-reassign': ['error', { props: false }],
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'es5',
+        singleQuote: true,
+      },
+    ],
     'jsx-a11y/anchor-is-valid': [
       'error',
       {
@@ -10,5 +33,6 @@ module.exports = {
         aspects: ['invalidHref', 'preferButton'],
       },
     ],
+    'no-unused-expressions': ['error', { allowTaggedTemplates: true }],
   },
 };
