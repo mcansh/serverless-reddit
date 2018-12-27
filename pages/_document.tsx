371fdfc049af3b38b9fa7425d969ca65286c2e5b
Logan McAnsh <logan@mcan.sh>
2018-12-27 15:38:03 -0500

    add lang to html tag
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import Document from 'next/document';
+import * as React from 'react';
+import Document, { NextDocumentContext, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx) {
+  static async getInitialProps(ctx: NextDocumentContext) {
     const sheet = new ServerStyleSheet();
 
     const originalRenderPage = ctx.renderPage;
     ctx.renderPage = () =>
       originalRenderPage({
+        // @ts-ignore
         enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
       });
 
@@ -18,5 +19,19 @@ export default class MyDocument extends Document {
       // @ts-ignore
       styles: [...initialProps.styles, ...sheet.getStyleElement()],
     };
+  }
+
+  render() {
+    const { styles } = this.props;
+
+    return (
+      <html lang="en">
+        <Head>{styles}</Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
   }
 }
