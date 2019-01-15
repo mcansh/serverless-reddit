7a1091d3342844ab8c1b3a9acbdda66d6bccdafc
Logan McAnsh <logan@mcan.sh>
2019-01-14 21:48:17 -0500

    update dependencies, enable serverless
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- next.config.js
+++ next.config.js
@@ -1,20 +1,8 @@
-const { PHASE_PRODUCTION_SERVER } =
-  process.env.NODE_ENV === 'development'
-    ? {}
-    : !process.env.NOW_REGION
-    ? require('next/constants')
-    : require('next-server/constants');
+const withTypescript = require('@zeit/next-typescript');
 
 const config = {
   crossOrigin: 'anonymous',
+  target: 'serverless',
 };
 
-module.exports = phase => {
-  if (phase === PHASE_PRODUCTION_SERVER) {
-    return config;
-  }
-
-  const withTypescript = require('@zeit/next-typescript');
-
-  return withTypescript(config);
-};
+module.exports = withTypescript(config);
