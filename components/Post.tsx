21d6479a28d2de124148a91df0c2d27c2639f67c
Logan McAnsh <logan@mcan.sh>
2019-02-04 14:18:01 -0500

    Revert "truncate full post title instead of clipping it at 60 characters"
    This reverts commit 428e2c22db466f7804fdf368af63e6b72184f89b.

--- components/Post.tsx
+++ components/Post.tsx
@@ -103,7 +103,7 @@ const PostStyles = styled(StyledLink).attrs({
     text-align: left;
     font-weight: bold;
     width: 80%;
-    white-space: nowrap;
+    white-space: pre;
     overflow: hidden;
     text-overflow: ellipsis;
   }
@@ -199,7 +199,7 @@ const Post = ({ post }: { post: Props }) => {
           <div className="feed-item__info">
             <div className="feed-item__header">
               <h2 className="feed-item__heading" title={post.title}>
-                {post.title}
+                {post.title.slice(0, 60)}
               </h2>
               <span className="feed_item__short-link">{url}</span>
             </div>
