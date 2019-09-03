c60c3e5bb94b625de2e0511c0aeba2869cec0be3
Logan McAnsh <logan@mcan.sh>
2019-09-03 19:18:03 -0400

    feat: forward rest of query params
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- pages/api/r/[subreddit]/[sort].ts
+++ pages/api/r/[subreddit]/[sort].ts
@@ -1,9 +1,10 @@
+import { format } from 'url';
 import { NextApiRequest, NextApiResponse } from 'next';
 import got from 'got';
 import { getFirstParam } from '~/utils/get-first-param';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const { subreddit, sort } = req.query;
+  const { subreddit, sort, ...query } = req.query;
 
   const feeds = ['hot', 'new', 'controversial', 'top', 'rising'];
 
@@ -12,10 +13,15 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       .status(400)
       .json({ message: `Sort '${sort}' not one of ${feeds.join(', ')}` });
   }
+
+  const url = format({
+    host: process.env.API_BASE,
+    pathname: `/r/${subreddit}/${sort}.json`,
+    query,
+  });
+
   try {
-    const response = await got(
-      `${process.env.API_BASE}/r/${subreddit}/${sort}.json`
-    );
+    const response = await got(url);
     res.json(response.body);
   } catch (error) {
     res.status(500);
