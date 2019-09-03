b2e5b74ff4a02e1b92889371542fa4a51e001f4c
Logan McAnsh <logan@mcan.sh>
2019-09-03 19:18:03 -0400

    feat: forward rest of query params
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- pages/api/r/[subreddit]/index.ts
+++ pages/api/r/[subreddit]/index.ts
@@ -1,11 +1,18 @@
+import { format } from 'url';
 import { NextApiRequest, NextApiResponse } from 'next';
 import got from 'got';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const { subreddit } = req.query;
+  const { subreddit, ...query } = req.query;
+
+  const url = format({
+    host: process.env.API_BASE,
+    pathname: `/r/${subreddit}.json`,
+    query,
+  });
 
   try {
-    const response = await got(`${process.env.API_BASE}/r/${subreddit}.json`);
+    const response = await got(url);
     res.json(response.body);
   } catch (error) {
     res.status(500);
--- pages/api/r/index.ts
+++ pages/api/r/index.ts
@@ -1,9 +1,16 @@
+import { format } from 'url';
 import { NextApiRequest, NextApiResponse } from 'next';
 import got from 'got';
 
-export default async (_req: NextApiRequest, res: NextApiResponse) => {
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const url = format({
+    host: process.env.API_BASE,
+    pathname: '/.json',
+    query: req.query,
+  });
+
   try {
-    const response = await got(`${process.env.API_BASE}/.json`);
+    const response = await got(url);
 
     res.json(response.body);
   } catch (error) {
