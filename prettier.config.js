99dbd11f5738f3c4022a8e798ae92e18f70938f5
Logan McAnsh <logan@mcan.sh>
2019-11-10 15:59:41 -0500

    chore(deps): bump to latest
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- prettier.config.js
+++ prettier.config.js
@@ -1,11 +1,3 @@
 module.exports = {
   ...require('eslint-config-mcansh/prettier.config'),
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
-      options: {
-        parser: 'babel-ts',
-      },
-    },
-  ],
 };
