f5aa5ff1556e6c2de68108321f49dcd2e352bbb6
Logan McAnsh <logan@mcan.sh>
2020-05-05 19:07:07 -0400

    feat: use incremental ssg instead of ssr
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- pages/r/[subreddit]/index.tsx
+++ pages/r/[subreddit]/index.tsx
@@ -1,2 +1,7 @@
+import { GetStaticPaths } from 'next';
+
 export { default } from '~/pages/index';
 export * from '~/pages/index';
+
+export const getStaticPaths: GetStaticPaths = () =>
+  new Promise(resolve => resolve({ paths: [], fallback: true }));
